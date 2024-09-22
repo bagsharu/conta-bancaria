3f9838fb25ddb85f35c8758b9e5b0de94334c708
Haru <gabs.haru@gmail.com>
2024-09-21 20:42:20 -0400

    Comentários

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,14 @@ public class Main {
         // Inicialização do Scanner
         Scanner scanner = new Scanner(System.in);
 
-
+        // Inicialização de variáveis de controle
         int comandoMenu = 0;
         double valorReceber, valorTransferir;
+
+        // Laço while que confirma as operações do usuário
         while (comandoMenu != 4) {
 
+            // Impressão do menu
             System.out.println("\nOperações\n");
             System.out.println("1- Consultar saldo");
             System.out.println("2- Receber valor");
@@ -32,18 +35,23 @@ public class Main {
             System.out.println("4- Sair");
             System.out.println("\nDigite a operação desejada:");
 
+            // Recebe o comando do usuário
             comandoMenu = scanner.nextInt();
             switch (comandoMenu){
-                case 1 :
+                case 1 : // Caso 1 seja inserido, imprime o status do saldo da conta.
                     System.out.printf("O saldo atual da conta é R$ %.2f", saldoCliente);
                     break;
-                case 2 :
+                case 2 : // Caso 2 seja inserido, inicia uma operação de receber dinheiro
+                         // O usuário informa o valor recebido que depois é somado no saldo da conta.
                     System.out.println("Informe o valor a receber:");
                     valorReceber = scanner.nextDouble();
                     saldoCliente += valorReceber;
                     System.out.printf("Saldo atualizado! O saldo atual é R$ %.2f", saldoCliente);
                     break;
                 case 3 :
+                         // Caso 3 seja inserido, inicia uma operação de transferir dinheiro
+                         // O usuário informa o valor a ser transferido e, caso a transferência seja
+                         // válida, remove o saldo da conta.
                     System.out.println("Informe o valor a transferir");
                     valorTransferir = scanner.nextDouble();
                     if (valorTransferir > saldoCliente) {
@@ -55,8 +63,10 @@ public class Main {
                     }
                     break;
                 case 4 :
+                         // Simplesmente finaliza as operações com uma mensagem.
                     System.out.println("\nOperações finalizadas!");
                 default:
+                         // Caso o valor inserido não seja uma operação válida, o usuário deve inserir novamente.
                     System.out.println("\nOperação inválida, tente novamente.");
             }
         }
