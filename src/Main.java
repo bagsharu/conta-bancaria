9959c014a862b1f4d91ffccba418b84ca96727fc
Haru <gabs.haru@gmail.com>
2024-09-21 20:12:31 -0400

    Impressão dos Dados do Cliente

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Inicialização dos Dados do Cliente:
+        String nomeCliente = "Heli Ribeiro";
+        String tipoConta = "Corrente";
+        double saldoCliente = 59631.13;
+
+        // Impressão dos dados do cliente:
+        System.out.println("*********************************************");
+        System.out.println("\nNome do cliente: " + nomeCliente);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo da Conta: " + saldoCliente);
+        System.out.println("\n*********************************************");
     }
 }
