a9109c280ba744b98afb4e5416d19f49143df201
Haru <gabs.haru@gmail.com>
2024-09-21 20:36:30 -0400

    Implementação do menu e suas funcionalidades

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -14,5 +16,49 @@ public class Main {
         System.out.println("Tipo de Conta: " + tipoConta);
         System.out.println("Saldo da Conta: " + saldoCliente);
         System.out.println("\n*********************************************");
+
+        // Inicialização do Scanner
+        Scanner scanner = new Scanner(System.in);
+
+
+        int comandoMenu = 0;
+        double valorReceber, valorTransferir;
+        while (comandoMenu != 4) {
+
+            System.out.println("\nOperações\n");
+            System.out.println("1- Consultar saldo");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- Sair");
+            System.out.println("\nDigite a operação desejada:");
+
+            comandoMenu = scanner.nextInt();
+            switch (comandoMenu){
+                case 1 :
+                    System.out.printf("O saldo atual da conta é R$ %.2f", saldoCliente);
+                    break;
+                case 2 :
+                    System.out.println("Informe o valor a receber:");
+                    valorReceber = scanner.nextDouble();
+                    saldoCliente += valorReceber;
+                    System.out.printf("Saldo atualizado! O saldo atual é R$ %.2f", saldoCliente);
+                    break;
+                case 3 :
+                    System.out.println("Informe o valor a transferir");
+                    valorTransferir = scanner.nextDouble();
+                    if (valorTransferir > saldoCliente) {
+                        System.out.println("Transferência negada, saldos insuficientes");
+                    }
+                    else {
+                        saldoCliente = saldoCliente - valorTransferir;
+                        System.out.println("Transferência realizada! O saldo atual é R$ " + saldoCliente);
+                    }
+                    break;
+                case 4 :
+                    System.out.println("\nOperações finalizadas!");
+                default:
+                    System.out.println("\nOperação inválida, tente novamente.");
+            }
+        }
     }
 }
